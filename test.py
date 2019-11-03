052996cbb591c95f181b385343f80ebddbfc0383
Jamie <jamcc09@gmail.com>
2019-11-03 22:52:26 +0100

    Extracted direction of movement vectors and plotted, need to account for direction of play though

--- test.py
+++ test.py
@@ -32,3 +32,26 @@ plt.show()
 # TODO (1) Plot vectors with orientation and direction of movement
 #      (2) Scale direction of movement vectors by speed and accel
 #      (3) Calculate rusher space between all defenders
+
+# need to convert to radians
+degs = data['Dir']
+rads = degs * (np.pi/180)
+
+# getting x, y components on the unit circle
+# will scale by speed at some point
+x_dirs = np.array(np.cos(rads)).reshape(-1, 1)
+y_dirs = np.array(np.sin(rads)).reshape(-1, 1)
+dir_vecs = np.concatenate([x_dirs, y_dirs], axis=1)
+
+# getting the origin of the vectors to plot, i.e. player positions
+x_pos = np.array(data['X']).reshape(-1,1)
+y_pos = np.array(data['Y']).reshape(-1,1)
+pos = np.concatenate([x_pos, y_pos], axis=1)
+
+# plot the vectors ...
+# not quite correct, need to take into account direction of play
+plt.quiver(
+    pos[0:22, 0], pos[0:22, 1],
+    dir_vecs[0:22, 0], dir_vecs[0:22, 1],
+    color=['r']*11 + ['b']*11, scale=21
+)
