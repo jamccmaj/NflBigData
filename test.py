ec79ad557dd76057f73693f8a290422b5d645843
Jamie <jamcc09@gmail.com>
2019-11-04 10:11:07 +0100

    Play can be chosen to plot based on cli arg

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python3
 
 import os
+import sys
 
 import numpy as np
 import pandas as pd
 
 from matplotlib import pyplot as plt
+
+play = int(sys.argv[1])
 
 homedir = os.environ['HOME']
 datapath = os.path.join(
@@ -15,43 +18,51 @@ datapath = os.path.join(
 data = pd.read_csv(datapath, low_memory=False)
 print(data.head())
 
-# get id of rusher on the first play of the first game
+play_data = data[play*22: (play+1)*22]
+
 rusher = np.argmax(
-    np.array(
-        data.head(n=22)['NflId'] == data.head(n=22)['NflIdRusher']
-    )
+    np.array(play_data['NflId'] == play_data['NflIdRusher'])
 )
 
 color_ids = ['red'] * 11 + ['blue'] * 11
 color_ids[rusher] = 'green'
 
 # show player position in first play in scatter plot with colors
-plt.scatter(data['X'][0:22], data['Y'][0:22], color=color_ids)
-plt.show()
+plt.scatter(play_data['X'], play_data['Y'], color=color_ids)
+# plt.show()
 
 # TODO (1) Plot vectors with orientation and direction of movement
 #      (2) Scale direction of movement vectors by speed and accel
 #      (3) Calculate rusher space between all defenders
+#      (4) Calculate player influence using basic bivariate normal
 
 # need to convert to radians
-degs = data['Dir']
-rads = degs * (np.pi/180)
+degs_dir = play_data['Dir']
+rads_dir = degs_dir * (np.pi/180)
 
 # getting x, y components on the unit circle
 # will scale by speed at some point
-x_dirs = np.array(np.cos(rads)).reshape(-1, 1)
-y_dirs = np.array(np.sin(rads)).reshape(-1, 1)
+x_dirs = np.array(np.cos(rads_dir)).reshape(-1, 1)
+y_dirs = np.array(np.sin(rads_dir)).reshape(-1, 1)
 dir_vecs = np.concatenate([x_dirs, y_dirs], axis=1)
 
 # getting the origin of the vectors to plot, i.e. player positions
-x_pos = np.array(data['X']).reshape(-1,1)
-y_pos = np.array(data['Y']).reshape(-1,1)
+x_pos = np.array(play_data['X']).reshape(-1,1)
+y_pos = np.array(play_data['Y']).reshape(-1,1)
 pos = np.concatenate([x_pos, y_pos], axis=1)
 
 # plot the vectors ...
 # not quite correct, need to take into account direction of play
 plt.quiver(
-    pos[0:22, 0], pos[0:22, 1],
-    dir_vecs[0:22, 0], dir_vecs[0:22, 1],
-    color=['r']*11 + ['b']*11, scale=21
+    pos[:, 0], pos[:, 1],
+    dir_vecs[:, 0], dir_vecs[:, 1],
+    color=color_ids, scale=50
 )
+plt.show()
+
+# TODO -- Project: Calculating player influence
+# (1) Generate mesh grid, which will be image pixels
+# (2) Calculate influence for each player and point on the image
+# Note: should be 11 images for defense, 10 for offense, still deciding for ball carrier
+# (3) Add images together for defense and offense separately
+# (4) Take difference between summed images for defense and offense, sigmoid it for a single image
