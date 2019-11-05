aee0c3ab672211785e2709531534212d66fad71b
Jamie <jamcc09@gmail.com>
2019-11-05 09:45:18 +0100

    Adding player field influence calculation and image generation for single player.. just loop over others per play

--- test.py
+++ test.py
@@ -2,11 +2,13 @@
 
 import os
 import sys
+import itertools
 
 import numpy as np
 import pandas as pd
 
 from matplotlib import pyplot as plt
+from scipy.stats import multivariate_normal as mvn
 
 play = int(sys.argv[1])
 
@@ -77,3 +79,53 @@ plt.show()
 # Note: should be 11 images for defense, 10 for offense, still deciding for ball carrier
 # (3) Add images together for defense and offense separately
 # (4) Take difference between summed images for defense and offense, sigmoid it for a single image
+
+# Computation of influence area
+# Find factor of Cov = R * S * S * R^-1
+
+# adjust angle for standard unit circle
+theta = -(rads_dir[11] - (np.pi/2))
+# rotation matrix for above angle
+R = np.array(
+    [
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)]
+    ]
+)
+
+# compute eigenvalues using projection of speed onto unit eigen vectors
+Sx = play_data.iloc[11].S * np.cos(theta)
+Sy = play_data.iloc[11].S * np.sin(theta)
+S = np.array([[Sx, 0], [0, Sy]])
+
+Sigma = R @ S @ S @ np.linalg.inv(R)
+
+
+player_x_pos = play_data.iloc[11].X
+player_y_pos = play_data.iloc[11].Y
+mvn_for_player_test = mvn(mean=[0, 0], cov=Sigma)
+mvn_for_player = mvn(
+    mean=[player_y_pos, player_x_pos], cov=Sigma
+)
+
+
+image_wd, image_ht = 120, 57
+# just checking to make sure the pdf computation is correct
+# either uses centered mvn or one centered at player position
+player_influence_image = np.zeros((image_ht, image_wd))
+player_influence_image_test = np.zeros((image_ht, image_wd))
+
+x, y = player_x_pos, player_y_pos
+for i, (a, b) in enumerate(
+    itertools.product(range(image_ht), range(image_wd))
+):
+    player_influence_image_test[a, b] = mvn_for_player_test.pdf([y-a, x-b])
+    player_influence_image[a, b] = mvn_for_player.pdf([a, b])
+
+fig, ax = plt.subplots(2, sharex=True)
+ax[0].imshow(player_influence_image)
+ax[1].imshow(player_influence_image_test)
+fig.savefig("./data/player_influence_image.png", dpi=300)
+
+test = (player_influence_image == player_influence_image_test).sum()
+print(test, image_wd * image_ht)
