Rotation now produces correct player influence

test.py
@@ -73,11 +73,12 @@ plt.quiver(
     dir_vecs[:, 0], dir_vecs[:, 1],
     color=color_ids, scale=50
 )
-# plt.quiver(
-#     pos[:, 0], pos[:, 1],
-#     ort_vecs[:, 0], ort_vecs[:, 1],
-#     color=color_ids, scale=25
-# )
+
+plt.quiver(
+    pos[:, 0], pos[:, 1],
+    ort_vecs[:, 0], ort_vecs[:, 1],
+    color=color_ids, scale=25
+)
 
 plt.show()
 
@@ -93,6 +94,7 @@ plt.show()
 
 # adjust angle for standard unit circle
 theta = -(rads_dir[11] - (np.pi/2))
+
 # rotation matrix for above angle
 R = np.array(
     [
@@ -108,34 +110,21 @@ S = np.array([[Sx, 0], [0, Sy]])
 
 Sigma = R @ S @ S @ np.linalg.inv(R)
 
-
 player_x_pos = int(round(play_data.iloc[11].X))
 player_y_pos = int(round(play_data.iloc[11].Y))
-mvn_for_player_test = mvn(mean=[0, 0], cov=Sigma)
-mvn_for_player = mvn(
-    mean=[player_y_pos, player_x_pos], cov=Sigma
-)
 
-
+mvn_for_player = mvn(mean=[player_x_pos, player_y_pos], cov=Sigma)
 image_wd, image_ht = 120, 57
-# just checking to make sure the pdf computation is correct
-# either uses centered mvn or one centered at player position
+
 # need to account for difference in coordinates between numpy and nfl data
 player_influence_image = np.zeros((image_ht, image_wd))
-player_influence_image_test = np.zeros((image_ht, image_wd))
 
-x, y = player_x_pos, image_ht - player_y_pos
 for i, (a, b) in enumerate(
     itertools.product(range(image_ht), range(image_wd))
 ):
-    a = (image_ht - 1) - a
-    player_influence_image_test[a, b] = mvn_for_player_test.pdf([y-a, x-b])
-    player_influence_image[a, b] = mvn_for_player.pdf([a, b])
+    a = image_ht - a - 1
+    player_influence_image[a, b] = mvn_for_player.pdf([b, a])
 
-fig, ax = plt.subplots(2, sharex=True)
-ax[0].imshow(player_influence_image)
-ax[1].imshow(player_influence_image_test)
+fig, ax = plt.subplots(1)
+ax.imshow(player_influence_image, origin='lower')
 fig.savefig("./data/player_influence_image.png", dpi=300)
-
-test = (player_influence_image == player_influence_image_test).sum()
-print(test, image_wd * image_ht)