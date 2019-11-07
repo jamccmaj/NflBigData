b0997ccb19323a37eb1534bc9012e0277fe51014
Jamie <jamcc09@gmail.com>
2019-11-07 13:52:42 +0100

    Removed for loop in lieu of numpy for speedier image computation, also check inverse exists

--- utils/images.py
+++ utils/images.py
@@ -1,10 +1,11 @@
 
+import sys
 import itertools
 import numpy as np
 
 from scipy.stats import multivariate_normal as mvn
 
-from utils.math import deg_to_rad
+from utils.math import deg_to_rad, sigmoid
 
 def compute_covariance(player_data):
     dir_rad = deg_to_rad(player_data.Dir)
@@ -22,19 +23,40 @@ def compute_covariance(player_data):
     Sy = speed * accel * np.sin(dir_rad)
     S = np.array([[Sx, 0.0], [0.0, Sy]])
 
-    return R @ S @ S @ np.linalg.inv(R)
+    return R @ S @ S @ np.linalg.pinv(R)
+
 
 def compute_image(player_data, image_wd, image_ht):
     image = np.zeros((image_ht, image_wd))
     sigma = compute_covariance(player_data)
-    mvn_player = mvn(
-        mean=[player_data.X, player_data.Y], cov=sigma
-    )
 
-    for _, (a, b) in enumerate(
-        itertools.product(range(image_ht), range(image_wd))
-    ):
-        a = image_ht - a - 1
-        image[a, b] = mvn_player.pdf([b, a])
+    if np.linalg.cond(sigma) < (1 / sys.float_info.epsilon):
+
+        mvn_player = mvn(
+            mean=[player_data.X, player_data.Y], cov=sigma,
+        )
+
+        a, b = zip(*list(
+            itertools.product(range(image_ht), range(image_wd))
+        ))
+
+        image[a, b] = mvn_player.pdf(list(zip(b, a)))
 
     return image
+
+
+def compute_image_for_team(play_data, image_wd, image_ht):
+    images = np.zeros((len(play_data), image_ht, image_wd))
+
+    for x in range(len(play_data)):
+        images[x] = compute_image(
+            play_data.iloc[x], image_wd, image_ht
+        )
+
+    return images
+
+
+def compute_ctrl_prob(play_images):
+    return sigmoid(
+        play_images[:11].sum(axis=0) - play_images[11:].sum(axis=0)
+    )
